b5c92ff281ae2ba403cf3a46b7ff5f2d22e0e9d9
Timothy McKenna <mckenna.tim@gmail.com>
2014-05-25 13:46:22 -0400

    initial commit

---
+++ app/js/views/note_show.js
@@ -0,0 +1,16 @@
+
+"use strict";
+window.APP = window.APP || {};
+APP.NoteShowView = Backbone.View.extend({
+  // the constructor
+  initialize: function (options) {
+    this.note = options.note;
+  },
+
+  // populate the html to the dom
+  render: function () {
+    this.$el.html(_.template($('#showTemplate').html(), this.note.toJSON()));
+    return this;
+  }
+});
+
